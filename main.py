17e9bfbacf1646d1e8b9cf1b0c144af3ca9ef4f0
RigaPadelCommunity <rigapadelcommunity@gmail.com>
2025-05-20 10:32:14 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -2,11 +2,12 @@ import asyncio
 import os
 from aiogram import Bot, Dispatcher
 from aiogram.enums import ParseMode
+from aiogram.fsm.storage.memory import MemoryStorage
 from handlers import register_handlers
 
 async def main():
     bot = Bot(token=os.getenv("BOT_TOKEN"), parse_mode=ParseMode.HTML)
-    dp = Dispatcher()
+    dp = Dispatcher(storage=MemoryStorage())
     register_handlers(dp, bot)
     await dp.start_polling(bot)
 
